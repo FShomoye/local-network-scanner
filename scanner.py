740a858300460443427affa0591ceefcb6488775
Favour-S <o.shomoye2@ac.ncl.uk>
2025-07-31 16:09:54 +0100

    Import necessary libraries

--- scanner.py
+++ scanner.py
@@ -1,4 +1,6 @@
+#Importing necessary libraries
 import ipaddress
-network = ipaddress.ip_network("192.168.1.0/24",strict=False)
-for ip in network.hosts():
-    print(ip)
+import socket
+
+#Allows you to scan multiple devices at once (uses threads)
+import concurrent.futures
